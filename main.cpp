f8a57f79f9677cf06de216bf8f31fc952f5b07bc
Novak <novaldix@gmail.com>
2023-05-31 15:49:59 +0300

    18_5_2 added solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
-int main() {
+using std::cout;
+using std::endl;
 
+int getJumpsNumber(int stairRung, int jumpsNumber = 0) {
+    if (stairRung <= 1) return 1;
+
+    while (stairRung) {
+        jumpsNumber += getJumpsNumber(--stairRung);
+    }
+
+    return jumpsNumber;
+}
+
+int main() {
+    const unsigned int stairRung = 3;
+
+    cout << "jumps number: " << getJumpsNumber(stairRung) << endl;
 }
